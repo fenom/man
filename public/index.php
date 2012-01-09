88ec2659512296a22efd9f17cb84e67df0d9709a
fenom <fenom23@hotmail.com>
2012-01-09 19:38:54 +0000

    view refactor

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ $action=strtok("/")?:"index";
 call_user_func(array("controller_$controller","action_$action"));
 ob_start();
 foreach(scandir("../view")as$view)
-	is_file("../view/$view")and(include"../view/$view")and${strtok($view,".")}=ob_get_contents()and ob_clean();
-include"../view/$controller/$action.php";
+	is_file("../view/$view")and(include"view/$view")and${strtok($view,".")}=ob_get_contents()and ob_clean();
+include"view/$controller/$action.php";
 $content=ob_get_clean();
-include"view/layout.php";
+include"index.php";
