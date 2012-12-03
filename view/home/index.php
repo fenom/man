c752b41135ede8b2cc0536ed7327b71b0b9c4c0c
fenom <fenom23@hotmail.com>
2012-12-03 20:31:17 +0000

    form action

--- view/home/index.php
+++ view/home/index.php
@@ -1,4 +1,4 @@
-		<form><!--man<input type=radio name=type value=man checked>info<input type=radio name=type value=info<?=@$_REQUEST["type"]=="info"?" checked":""?>--><input name=q><input type=submit></form>
+		<form action=/><!--man<input type=radio name=type value=man checked>info<input type=radio name=type value=info<?=@$_REQUEST["type"]=="info"?" checked":""?>--><input name=q><input type=submit></form>
 		<pre>
 <?=htmlspecialchars($data["man"])?>
 		</pre>
