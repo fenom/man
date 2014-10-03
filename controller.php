eb119a7d060b852e2abafb358ef271b79af9f629
fenom <fenom23@hotmail.com>
2014-10-03 00:07:58 +0000

    page specific title

--- controller.php
+++ controller.php
@@ -10,7 +10,7 @@ class controller
 		$page or!isset($_REQUEST["q"])or header("location:/{$_REQUEST["q"]}")or exit;
 		$pages=preg_replace("/\.\d.*.gz/","",explode("\n",`ls -R1 /usr/share/man/|grep -e "\(.*\)\.*.*\.gz"`));
 		sort($pages);
-		return@array("data"=>array("man"=>shell_exec(escapeshellcmd(($_REQUEST["type"]=="info"?"info":"man")." $page")),"pages"=>array_unique($pages)));
+		return@array("data"=>array("man"=>shell_exec(escapeshellcmd(($_REQUEST["type"]=="info"?"info":"man")." $page")),"pages"=>array_unique($pages),"title"=>$_REQUEST["q"]));
 	}
 	function after()
 	{
--- view/partial/home/head.php
+++ view/partial/home/head.php
@@ -1,4 +1,4 @@
-		<title>man</title>
+		<title><?=$data["title"]?:"man"?></title>
 		<link href=/css/home.css rel=stylesheet>
 		<script src=/js/home.js></script>
 		<script src=http://code.jquery.com/jquery-latest.js></script>
